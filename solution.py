3fae37916aa33bc060c40949f50104824c86c42c
McSimik <80466309+McSimik@users.noreply.github.com>
2023-04-13 21:34:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 964993301 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    px=x_success/x_cnt
+    py=y_success/y_cnt
+    dis=px*(1-px)/x_cnt + py*(1-py)/y_cnt
+    
+    return (py-px)/np.sqrt(dis) >= norm.ppf(0.02)
